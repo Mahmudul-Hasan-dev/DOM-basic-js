0284b08ddc13b3b49e4cef8632a4b1b9a0ff6ef7
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-12 12:33:26 +0600

    element by tag done

--- js/dom.js
+++ js/dom.js
@@ -2,3 +2,25 @@ console.log('js connected');
 
 //we can access the whole html using document in the console.log()
 console.log(document);
+
+const student = {
+    name: 'abul',
+    ID: 21,
+    age: 25,
+    study: function (time) {
+        console.log(time, 'stduying')
+    }
+}
+
+console.log(document.getElementsByTagName('h3'))
+let licollection = document.getElementsByTagName('li');
+// console.log(licollection);
+for (const li of licollection) {
+    console.log(li.innerText);
+}
+
+let headings = document.getElementsByTagName('h3');
+// console.log(headings);
+for (const heading of headings) {
+    console.log(heading.innerText);
+}
