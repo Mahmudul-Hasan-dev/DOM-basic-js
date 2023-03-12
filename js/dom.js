45cdc611eb1307ae817bd9ecc241233a3aa1ec79
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-12 13:36:18 +0600

    done

--- js/dom.js
+++ js/dom.js
@@ -1,26 +1,48 @@
 console.log('js connected');
 
 //we can access the whole html using document in the console.log()
-console.log(document);
+// console.log(document);
 
-const student = {
-    name: 'abul',
-    ID: 21,
-    age: 25,
-    study: function (time) {
-        console.log(time, 'stduying')
-    }
-}
+// const student = {
+//     name: 'abul',
+//     ID: 21,
+//     age: 25,
+//     study: function (time) {
+//         console.log(time, 'stduying')
+//     }
+// }
 
-console.log(document.getElementsByTagName('h3'))
-let licollection = document.getElementsByTagName('li');
-// console.log(licollection);
-for (const li of licollection) {
-    console.log(li.innerText);
-}
+// console.log(document.getElementsByTagName('h3'))
+// let licollection = document.getElementsByTagName('li');
+// // console.log(licollection);
+// for (const li of licollection) {
+//     console.log(li.innerText);
+// }
 
-let headings = document.getElementsByTagName('h3');
-// console.log(headings);
-for (const heading of headings) {
-    console.log(heading.innerText);
+// let headings = document.getElementsByTagName('h3');
+// // console.log(headings);
+// for (const heading of headings) {
+//     console.log(heading.innerText);
+// }
+
+//get element by ID
+let menuTitle = document.getElementById('menu-title');
+console.log(menuTitle.innerText);
+
+//if we want to change the innertext
+menuTitle.innerText = 'js domination';
+
+//get element by className
+let changeColor = document.getElementsByClassName('important-places');
+console.log(changeColor);
+
+
+//by query SelectorAll
+// console.log(document.querySelectorAll('.fruits-container li'));
+
+let allfruit = document.querySelectorAll('.fruits-container li');
+console.log(allfruit.innerText);
+for (let fruit of allfruit) {
+    console.log(fruit.innerText);
 }
+// console.log(document.querySelector('.fruits-container li'));
